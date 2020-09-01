418250ab32438bd1f7206743b2a9c8c63e2aaf20
Alexey Kudryashov <akud.soft@gmail.com>
2020-09-01 16:14:52 +0400

    fix collapse

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
-$(document).mouseup(function(e) {
-    var div = $(".modal-dialog");
-    if (!div.is(e.target)
-        &&
-        div.has(e.target).length === 0) {
-        div.closes('.modal').removeClass('show');
+const expandableBlock = (button, textBlock, defaultHeight = 63) => {
+  $(button).on('click', (e) => {
+    const thisText = $(e.currentTarget).siblings(`.${textBlock}`);
+    console.log($(thisText).hasClass(`${textBlock}--show`));
+    if($(thisText).hasClass(`${textBlock}--show`)) {
+      $(thisText).animate({
+        height: `${defaultHeight}px`
+      }, 0, function () {
+        $(thisText).removeClass(`${textBlock}--show`);
+      })
+    } else {
+      const paragraphs = $(thisText).find('p');
+      let height = 0;
+      paragraphs.map((index, element) => {
+        console.log(element, $(element).outerHeight(true));
+        height += $(element).outerHeight(true);
+      });
+      $(thisText).animate({
+        height: `${height}px`
+      }, 0, function () {
+        $(thisText).addClass(`${textBlock}--show`);
+      })
     }
+  })
+}
+
+$(document).mouseup(function (e) {
+  const div = $('.modal-dialog')
+  if(!div.is(e.target) && div.has(e.target).length === 0) {
+    div.closes('.modal').removeClass('show');
+  }
 });
 
 $(document).ready(() => {
-    $('.nav-link').on('click', () => {
-        const data = $(this).data('toggle');
-        if (data === 'modal') {
-            if ($('.modal').hasClass('show')) {
+  $('.nav-link').on('click', () => {
+    const data = $(this).data('toggle');
+    if(data === 'modal') {
+      if($('.modal').hasClass('show')) {
 
-            }
-        }
-    });
+      }
+    }
+  });
+  expandableBlock('.shop-gifts__more', 'shop-gifts__text');
+  expandableBlock('.answer__item-more', 'answer__item-text', 125);
 });
